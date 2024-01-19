627e25086ac60e9d06825772dc922f9987b91d06
Jonatan Andrés Muñoz Casallas <jonatan.munoz@mawiapps.com.co>
2024-01-18 20:51:57 -0500

    primer commit

---
+++ web_site_jamcawmi/homepage/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, "homepage/home.html")
+
+def about(request):
+    return render(request, 'homepage/about.html')
+
+def services(request):
+    return render(request, 'homepage/services.html')
+
+def portafolio(request):
+    return render(request, 'homepage/portafolio.html')
+
+def faq(request):
+    return render(request, 'homepage/faq.html')
+
+def contact(request):
+    return render(request, 'homepage/contact.html')
